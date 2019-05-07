912e5d33064604ad182b9fae4d32390c84e9faa0
alexislopes <alexisdrops@gmail.com>
2019-05-07 16:54:02 -0300

    started client side

--- services/passport.js
+++ services/passport.js
@@ -21,14 +21,14 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL:  '/auth/google/callback',
     proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({googleId: profile.id})
-        .then((existingUser) => {
-            if(existingUser){
-                done(null, existingUser)
-            } else {
-                new User({googleId: profile.id}).save()
-                .then(user => done(null, user))
-            }
-        })
+}, async (accessToken, refreshToken, profile, done) => {
+    const existinUser = await User.findOne({googleId: profile.id})
+    
+    if(existingUser){
+        done(null, existingUser)
+    } else {
+        const user = await new User({googleId: profile.id}).save()
+        done(null, user)
+    }
+      
 }))
